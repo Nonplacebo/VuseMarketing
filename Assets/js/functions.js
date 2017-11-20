6428f04c5975e3b28cf63507544483a9bc000d3b
Dustin Black <dustnblack@gmail.com>
2017-11-20 11:48:05 -0500

    design COMPLETE

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -7,13 +7,53 @@ $(function() {
     });
 
 
+
+    //$('.showcase').slick({
+      //infinite: true,
+      //slidesToShow: 3,
+      //slidesToScroll: 3,
+      //autoplay: true,
+      //arrows: false,
+      //draggable: false
+    //});
+
     $('.showcase').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      autoplay: true,
-      arrows: false,
-      draggable: false
-    });
+  autoplay: true,
+  draggable: false,
+  arrows: false,
+  dots: false,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 });
